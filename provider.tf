65a4c7679de8fdc8eb7527b36437a80d9b0d9dcc
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2022-11-09 15:40:17 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA6JWWL57AW3JMTFVQ"
+  secret_key = "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"
+  
+}
